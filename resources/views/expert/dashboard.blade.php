416790c8d486434221ac8ebe61cc0451a6620ebe
shakil <kazimuhammadullah@gmail.com>
2024-03-27 22:16:51 +0600

    dse

--- resources/views/expert/dashboard.blade.php
+++ resources/views/expert/dashboard.blade.php
@@ -1,1 +1,28 @@
-<h1>Expert Successfully Logged In.</h1>
+@extends('expert.layouts.health')
+@section('content')
+
+  {{-- <h1>Dashboard Coming Soon ..</h1>   --}}
+
+  
+  <section class="header-main py-5">
+    <div class="container ">
+        <div class="col-lg-10 mx-auto px-4 ">
+            <div class="row">
+
+              {{-- @foreach ($dusers as $item)
+              <div class="alert alert-info" role="alert">
+                <iconify-icon icon="flat-color-icons:idea"></iconify-icon> You have new user. please check this  <a href="{{route('linemanager.userlist')}}">link</a> <em class="text-dark fw-bold"></em>
+             </div>
+              @endforeach
+              <div class="alert alert-info" role="alert">
+                <iconify-icon icon="flat-color-icons:idea"></iconify-icon> You have new user. please check this  <a href="{{route('linemanager.userlist')}}">link</a> <em class="text-dark fw-bold"></em>
+             </div> --}}
+
+                
+            </div>
+
+        </div>
+    </div>
+</section>
+  
+@endsection
---
+++ resources/views/expert/layouts/master.blade.php
@@ -0,0 +1,34 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DSE</title>
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/app.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <style>
+        .hidden {
+            display: none;
+        }
+    </style>
+</head>
+
+<body>
+
+    @yield('content')
+
+    <!-- jQuery -->
+    <script src="{{ asset('assets/admin/js/jquery.min.js')}}"></script>
+    <script src="https://code.iconify.design/iconify-icon/2.0.0/iconify-icon.min.js"></script>
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/app.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    
+    @yield('script')
+
+</body>
+
+</html>
