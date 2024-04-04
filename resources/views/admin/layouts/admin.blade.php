f22128436bd5f10af73e24d43200b4d66042c30c
shakil <kazimuhammadullah@gmail.com>
2024-04-04 12:33:25 +0600

    dse design

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -3,6 +3,8 @@
 
 <head>
   <meta charset="utf-8">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>DSE - Admin</title>
